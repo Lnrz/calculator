5388526ad2539d0472358f5213fc219688dfd532
Lorenzo <loreevil@gmail.com>
2021-09-03 17:49:12 +0200

    Linkati bottoni e funzioni, migliorato aspetto

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,10 @@ function divide(x, y) {
 }
 
 function operate(x, y, op) {
+    
+    x = parseFloat(x);
+    y = parseFloat(y);
+
     switch (op) {
         case "+":
             return add(x, y);
@@ -30,6 +34,84 @@ function operate(x, y, op) {
             }
     }
 }
+
+function addNumeral(obj) {
+    
+    if (nextNumber) {
+        currentIndex++;
+        nextNumber = false;
+    }
+
+
+
+    totalInput[currentIndex] = (totalInput[currentIndex] == "Impossibile") ? "" :
+        totalInput[currentIndex]
+        + obj.target.textContent
+    ;
+
+    screen.textContent = totalInput[currentIndex];
+}
+
+function chooseOperation(obj) {
+    
+    if (totalInput[2] != "") {
+        equal();
+    }
+
+    if (!nextNumber) {
+        currentIndex++;
+    }
+
+    totalInput[currentIndex] = obj.target.textContent;
+
+    nextNumber = true;
+}
+
+function equal() {
+
+    nextNumber = false;
+    currentIndex = 0;
+    
+    let result = operate(totalInput[0], totalInput[2], totalInput[1]);
+
+    screen.textContent = result;
+    totalInput[0] = "" + (result == "Impossibile")? "" : result;
+    totalInput[1] = "";
+    totalInput[2] = "";
+}
+
+function clear() {
+
+    totalInput[0] = "";
+    totalInput[1] = "";
+    totalInput[2] = "";
+    currentIndex = 0;
+    nextNumber = false;
+    screen.textContent = "";
+}
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const input = [];
+const totalInput = ["", "", ""];
+let currentIndex = 0;
+let nextNumber = false;
+const screen = document.querySelector("#screen");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(obj => {
+    switch (obj.textContent) {
+        case "+":
+        case "-":
+        case "*":
+        case "/":   obj.addEventListener("click", chooseOperation);
+            break;
+        case "=":   obj.addEventListener("click", equal);
+            break;
+        case ".":
+            break;
+        case "Clear":   obj.addEventListener("click", clear)
+            break;
+        default:    obj.addEventListener("click", addNumeral);
+            break;
+    }
+});
