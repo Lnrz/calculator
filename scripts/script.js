f859d897d41777c38fddc22ebba4c4c44ba0f77f
Lorenzo <loreevil@gmail.com>
2021-09-05 18:35:22 +0200

    Incominciato sistema per la gestione di numeri con più di 16 cifre

--- scripts/script.js
+++ scripts/script.js
@@ -54,6 +54,10 @@ function addNumeral(obj) {
 
 function chooseOperation(obj) {
     
+    if (totalInput[0] == "") {
+        return;
+    }
+
     if (totalInput[2] != "") {
         equal();
     }
@@ -64,6 +68,7 @@ function chooseOperation(obj) {
 
     totalInput[currentIndex] = obj.target.textContent;
 
+    dotButton.addEventListener("click", addNumeral, {once: true});
     nextNumber = true;
 }
 
@@ -71,11 +76,12 @@ function equal() {
 
     nextNumber = false;
     currentIndex = 0;
+    dotButton.addEventListener("click", addNumeral, {once: true});
     
     let result = operate(totalInput[0], totalInput[2], totalInput[1]);
 
-    screen.textContent = result;
-    totalInput[0] = "" + (result == "Impossibile")? "" : result;
+    screen.textContent = adjustLength(result);
+    totalInput[0] = (result == "Impossibile")? "" : result;
     totalInput[1] = "";
     totalInput[2] = "";
 }
@@ -88,6 +94,76 @@ function clear() {
     currentIndex = 0;
     nextNumber = false;
     screen.textContent = "";
+    dotButton.addEventListener("click", addNumeral, {once: true});
+}
+
+function delApprox(n, t) {
+
+    let fracPartLength = n.split(".")[1].length; 
+
+    return parseFloat(n).toFixed(fracPartLength - t).toString();
+}
+
+function adjustLength(n) {
+
+    n = n.toString();
+    diff = n.length - 16;
+
+    if (diff > 0) {
+
+        while (diff > 0 && n.includes(".")) {
+
+            n = delApprox(n, 1);
+            diff--;
+
+            if (!n.includes(".")) {
+                diff--;
+            }
+        }
+
+        if (diff > 0) {
+
+            let mantissa = delApprox(("0." + n.slice(1)), 5).slice(2);
+            
+            let mantissaLength = mantissa.length;
+
+            while (mantissa.charAt(mantissa.length - 1) == "0") {
+                
+                mantissa = mantissa.slice(0, mantissa.length - 1);
+                diff--;
+            }
+            //da spostare a dopo e togliere quella variabile
+
+            let arr = [n.charAt(0) + "." + mantissa,
+                (mantissaLength + 5).toString()]
+            ;
+            
+            let expLength = arr[1].length;
+
+            diff--;
+            
+            while (diff > 0) {
+
+                arr[0] = delApprox(arr[0], 1);
+                arr[1] = +arr[1] + 1;
+
+                if (arr[1].length > expLength) {
+
+                    expLength = arr[1].length - expLength;
+
+                    arr[0] = delApprox(arr[0], expLength);
+
+                    expLength = arr[1].length;
+                }
+                
+                diff--;
+            }
+
+            n = arr[0] + "e" + arr[1];
+        }
+    }
+    return n;
+
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -95,6 +171,7 @@ const totalInput = ["", "", ""];
 let currentIndex = 0;
 let nextNumber = false;
 const screen = document.querySelector("#screen");
+const dotButton = document.querySelector("#dot");
 
 const buttons = document.querySelectorAll("button");
 
@@ -107,9 +184,9 @@ buttons.forEach(obj => {
             break;
         case "=":   obj.addEventListener("click", equal);
             break;
-        case ".":
-            break;
         case "Clear":   obj.addEventListener("click", clear)
+            break;
+        case ".":   obj.addEventListener("click", addNumeral, {once: true})
             break;
         default:    obj.addEventListener("click", addNumeral);
             break;
